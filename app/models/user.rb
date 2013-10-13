cd085cc3b27bd5002dda8f575230afa1c542fdfe
Armando Mendoza <fcoarmandomendoza@gmail.com>
2013-10-12 20:12:11 -0430

    Added callback to model User to set default user_type.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   #### Constants ####
-  USER_TYPE = { super_admin: "super_admin", admin: "admin", owner: "owner",
-    regular: "regular" }
+  USER_TYPE = { admin: "admin", owner: "owner", regular: "regular" }
 
   #### Relations ####
   has_many :collaborators
   has_many :projects, through: :collaborators
   belongs_to :company, polymorphic: true
 
+  #### Callbacks ####
+  before_save :set_user_type
+
+  def set_user_type
+    self.user_type ||= USER_TYPE[:regular]
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,11 @@ describe User do
   it { should have_many(:collaborators) }
   it { should have_many(:projects).through(:collaborators) }
 
+  describe "Callback#set_user_type" do
+    it "if user_type is blank it set User::USER_TYPE[:regular] by default" do
+      user = User.create!(first_name: "Armando", last_name: "Mendoza", password: "12345678",
+        company: Contractor.make!, email: "fcoarmando@hotmail.com")
+      expect(user.user_type).to eq(User::USER_TYPE[:regular])
+    end
+  end
 end
