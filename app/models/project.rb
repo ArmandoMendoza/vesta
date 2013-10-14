b1e194e91672c3ccc0ce77cb90ac4898eb90619c
Armando Mendoza <fcoarmandomendoza@gmail.com>
2013-10-13 23:00:37 -0430

    Added some default values to Proyect model.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
+  #### Constants ####
+  STATE = { executing: "executing", stopped: "stopped", finished: "finished" }
+
   #### Relations ####
   has_many :collaborators
   has_many :users, through: :collaborators
@@ -11,8 +14,21 @@ class Project < ActiveRecord::Base
   validates_numericality_of :budget, :advance, :contractor_id, :latitude, :longitude
   validate :check_init_date
 
+  #### Callbacks ####
+  before_validation :set_defaults
+  before_create :set_state
 
   private
+    def set_defaults
+      self.advance ||= 0
+      self.latitude ||= 0
+      self.longitude ||= 0
+    end
+
+    def set_state
+      self.state = STATE[:executing]
+    end
+
     def check_init_date
       if (init_date.present? && finish_date.present?) && (init_date > finish_date)
         errors[:init_date] = "fecha de Inicio debe ser menor a fecha de Culminacion"
